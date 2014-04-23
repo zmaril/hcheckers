8171a3b3a07ec879bbf6119118b9fd1eae9b5b4d
Zack Maril <zack@zacharymaril.com>
2014-04-23 15:16:30 -0500

    Moving things around and adding in more math slowly.

--- Main.hs
+++ Main.hs
@@ -11,17 +11,10 @@ import Debug.Trace as T
 import Data.List
 import Text.PrettyPrint.Boxes
 
-data Game = Game {i :: Int, reds :: [[Bool]]} deriving(Show)
-createGame :: Gr -> P.Partition -> P.Partition -> Game
-createGame Gr{k=ki, n=ni} a b = Game 0 []
+-- Small useful function
+pad xs m el = xs ++ replicate (m - length xs) el
 
-bubbledPairs :: Int -> [((Int,Int),(Int,Int))]
-bubbledPairs n =  [((i,y1),(1+x2,y1-1)) 
-                   | x2 <- [0..n-1]
-                   ,  i <- [0..x2]
-                   ,  let y1=n+i-x2 ]
-
-
+-- Grassmanians and functions
 data Gr = Gr {k::Int, n::Int} deriving (Show)
 
 partitionsFor Gr{k=ki, n=ni} =  
@@ -32,10 +25,43 @@ problemFor conds partition =  --There may be a more efficent way to do this
     nub $ map sort $ S.listTensor $ map (conds !!) $ map pred partition
 
 problemsFor g@Gr{k=ki, n=ni} = 
-      map (problemFor $ partitionsFor g) $  P._partitions $ ki*(ni-ki)
+      foldr (++) [] $ map (problemFor $ partitionsFor g) 
+                $  P._partitions $ ki*(ni-ki)
 
-pad xs m el = xs ++ replicate (m - length xs) el
 
+-- Game
+
+data Game = Game {i :: Int, reds :: [[Bool]]} deriving(Show)
+
+gameIndices xs = reverse $ filter (/= 0) $ zipWith (*) xs [length xs,length xs - 1..1]
+walkP h []     prev xs = xs ++ (replicate (h - prev) 1) 
+walkP h (p:ps) prev xs = 
+    T.trace (show h ++ " " ++ show p ++ " " ++ show ps ++ " " ++ show prev ++ " " ++ show xs) $
+     walkP h ps p (xs ++ (replicate (p - prev) 1) ++ [0])
+findCoords h l xs = 
+    let prepped = reverse (pad xs l 0)
+    in
+    gameIndices $ walkP h prepped 0 []
+
+
+
+--createGame :: Gr -> P.Partition -> P.Partition -> Game
+createGame Gr{k=ki, n=ni} a b = 
+    let ac = findCoords ki (ni-ki) a 
+        bc = findCoords ki (ni-ki) b
+        coords = zip ac (reverse bc) 
+    in
+      coords
+
+
+bubbledPairs :: Int -> [((Int,Int),(Int,Int))]
+bubbledPairs n =  [((i,y1),(1+x2,y1-1)) 
+                   | x2 <- [0..n-1]
+                   ,  i <- [0..x2]
+                   ,  let y1=n+i-x2 ]
+
+
+-- Pretty Boxes for printing things 
 mark  = char '#'
 blank = char ' '
 toCol k i = vcat left $ pad (replicate i mark) k blank
@@ -44,4 +70,4 @@ condsToBox g conds = hsep 1 center1 $ map (condToBox gr) conds
 
 gr = Gr 3 6
 --main = do printBox $ condsToBox gr $ foldr (++) [] $ problemsFor gr 
-main = do printBox $ vsep 1 left $ map (condsToBox gr) $ foldr (++) [] $ problemsFor gr
+main = do printBox $ vsep 1 left $ map (condsToBox gr) $ problemsFor gr
